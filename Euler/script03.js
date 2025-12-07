1a4d862d89f04d0fe166cc1420933c75f58088a3
MeatGamer <justas.brazaitis@stud.kitm.lt>
2025-12-07 23:32:43 +0200

    Update script03.js

--- Euler/script03.js
+++ Euler/script03.js
@@ -1,27 +1,14 @@
 // 20
-// masyvas
-let mass = [1];
-
-for (let i = 2; i <= 100; i++) {
-    // daugina is i
-    let left = 0
-    for (let j = 0; j < mass.length; j++) {
-        let abc = mass[j] * i + left
-        mass[j] = abc % 10 // paskutinis sk lieka
-        left = Math.floor(abc / 10) // perkele
-    }
-    // jei lieka prideda nauja sk
-    while (left > 0) {
-        mass.push(left % 10)
-        left = Math.floor(left / 10)
-    }
+let ats = 1n
+for (let i = 1n; i <= 100n; i++) {
+    ats *= i
 }
-// mass pavirsta atvirkscias
+//i string pavercia
+let tekstas = ats.toString()
 
-// suma
-let sum = 0;
-for (let i = 0; i < mass.length; i++) {
-    sum += mass[i]
+let suma = 0
+for (let i = 0; i < tekstas.length; i++) {
+    suma += parseInt(tekstas[i])
 }
 
-console.log(sum)
+console.log(suma)
